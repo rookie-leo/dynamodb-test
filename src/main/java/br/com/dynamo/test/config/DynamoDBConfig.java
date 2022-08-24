2451fba1b05b965e2997b2db2e85b391864586fa
rookie-leo <proleonarpa@gmail.com>
2022-08-24 09:24:13 -0300

    Implementado classe de configuração do DynamoDB

--- src/main/java/br/com/dynamo/test/config/DynamoDBConfig.java
+++ src/main/java/br/com/dynamo/test/config/DynamoDBConfig.java
@@ -1,4 +1,43 @@
 package br.com.dynamo.test.config;
 
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.util.StringUtils;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDynamoDBRepositories("br.com.dynamo.test.repositories")
 public class DynamoDBConfig {
+
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
+
+    @Value("${amazon.aws.accesskey}")
+    private String amazonAWSAccessKey;
+
+    @Value("${amazon.aws.secretkey}")
+    private String amazonAWSSecretKey;
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(amazonCredentials());
+
+        if (!StringUtils.isNullOrEmpty(amazonDynamoDBEndpoint)) {
+            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+        }
+
+        return amazonDynamoDB;
+    }
+
+    @Bean
+    private AWSCredentials amazonCredentials() {
+        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+    }
+
 }
